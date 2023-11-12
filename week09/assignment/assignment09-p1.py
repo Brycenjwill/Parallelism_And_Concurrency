b6c4b4fc238bda862419299dd92bd51137f8528b
Brycenjwill <114171494+Brycenjwill@users.noreply.github.com>
2023-11-12 10:57:37 -0700

    now can solve maze

--- week09/assignment/assignment09-p1.py
+++ week09/assignment/assignment09-p1.py
@@ -30,26 +30,34 @@ speed = SLOW_SPEED
 
 
 
-def solve_path(maze, Path, pos, tempPath, forks):
+def solve_path(maze, Path, pos):
     """ Solve the maze and return the path found between the start and end positions.  
         The path is a list of positions, (x, y) """
-        
     # TODO start add code here
-    possibleMoves = maze.get_possible_moves(pos[0], pos[1])
-    if maze.at_end(pos[0], pos[1]) == False:
-        if len(possibleMoves) == 1:
-            maze.move(possibleMoves[0][0], possibleMoves[0][1], (0,255,0))
-            tempPath.append([possibleMoves[0][0], possibleMoves[0][1]])
-            solve_path(maze, Path, [possibleMoves[0][0], possibleMoves[0][1]], tempPath, forks)
-        elif len(possibleMoves) > 1:
-            Path.append(tempPath)
-            tempPath = pos
-            solve_path(maze, Path, [possibleMoves[0][0], possibleMoves[0][1]], tempPath, forks)       
-        else:
-            solve_path(maze, Path, tempPath[0], tempPath, forks)      
-    return Path
-    
+    possibleMoves = maze.get_possible_moves(pos[0], pos[1]) #Check how many possible moves there are
+    while len(possibleMoves) >= 1:
+        possibleMoves = maze.get_possible_moves(pos[0], pos[1]) #Check how many possible moves there are
+        if maze.at_end(pos[0], pos[1]) == True: #If not at exit. . .
+            return Path
 
+        if maze.at_end(pos[0], pos[1]) != True: #If not at exit. . .
+            if len(possibleMoves) >= 1: #Logic for when not at end of Path
+                Path.append(possibleMoves[0]) #Add move taken to Path
+                pos = possibleMoves[0]
+                maze.move(possibleMoves[0][0], possibleMoves[0][1], (0,0,255)) #Move to next square
+
+    #When at a dead end...
+    ran = len(Path) - 1
+    for i in range(0, len(Path)-1): #Loop through the Path list backwards
+        index = i
+        position = Path[ran-index]
+        if len(maze.get_possible_moves(position[0], position[1])) >= 1: #Check to make sure position in loop isnt an intersection. . .
+            solve_path(maze, Path, position)
+            return Path
+            
+        else:
+            maze.restore(position[0], position[1])
+            Path.pop()
 
 
 
@@ -67,7 +75,7 @@ def get_path(log, filename):
 
     maze = Maze(screen, SCREEN_SIZE, SCREEN_SIZE, filename)
 
-    path = solve_path(maze, [], [0,1], [], [])
+    path = solve_path(maze, [[0,1]], [0,1],)
 
     log.write(f'Number of drawing commands for = {screen.get_command_count()}')
 
